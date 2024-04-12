4d9f3d49e90f13b29e235fd714aff58b71dca673
whoami13579 <liangforcoding@gmail.com>
2024-04-12 16:41:49 +0800

    modify CMakeLists.txt

---
+++ build/CMakeFiles/tic_tac_toe_minimax.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tic_tac_toe_minimax.dir/Board.cpp.o"
+  "CMakeFiles/tic_tac_toe_minimax.dir/Board.cpp.o.d"
+  "CMakeFiles/tic_tac_toe_minimax.dir/Game.cpp.o"
+  "CMakeFiles/tic_tac_toe_minimax.dir/Game.cpp.o.d"
+  "CMakeFiles/tic_tac_toe_minimax.dir/MiniMax.cpp.o"
+  "CMakeFiles/tic_tac_toe_minimax.dir/MiniMax.cpp.o.d"
+  "CMakeFiles/tic_tac_toe_minimax.dir/Piece.cpp.o"
+  "CMakeFiles/tic_tac_toe_minimax.dir/Piece.cpp.o.d"
+  "CMakeFiles/tic_tac_toe_minimax.dir/Texture.cpp.o"
+  "CMakeFiles/tic_tac_toe_minimax.dir/Texture.cpp.o.d"
+  "CMakeFiles/tic_tac_toe_minimax.dir/main.cpp.o"
+  "CMakeFiles/tic_tac_toe_minimax.dir/main.cpp.o.d"
+  "tic_tac_toe_minimax"
+  "tic_tac_toe_minimax.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tic_tac_toe_minimax.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
