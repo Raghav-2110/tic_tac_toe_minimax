29acc5ee1ef4e0debf483f2aa4737a09565fca07
whoami13579 <liangforcoding@gmail.com>
2024-06-24 01:29:18 +0800

    update Texture.cpp

--- Texture.cpp
+++ Texture.cpp
@@ -11,6 +11,6 @@ sf::Texture Texture::loadTexture(const std::string &filename) {
     return texture;
 }
 
-sf::Texture Texture::o = loadTexture("/Users/mba/myCode/tic-tac-toe-minimax/Texture/o.png");
-sf::Texture Texture::x = loadTexture("/Users/mba/myCode/tic-tac-toe-minimax/Texture/x.png");
-sf::Texture Texture::board = loadTexture("/Users/mba/myCode/tic-tac-toe-minimax/Texture/board.png");
+sf::Texture Texture::o = loadTexture("/Users/mba/Code/tic-tac-toe-minimax/Texture/o.png");
+sf::Texture Texture::x = loadTexture("/Users/mba/Code/tic-tac-toe-minimax/Texture/x.png");
+sf::Texture Texture::board = loadTexture("/Users/mba/Code/tic-tac-toe-minimax/Texture/board.png");
